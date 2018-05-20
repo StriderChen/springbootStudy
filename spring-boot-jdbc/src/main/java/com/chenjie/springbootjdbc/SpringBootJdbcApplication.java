aa09922146e51e30253bc66d240a65533731ec56
myGit <729217667@qq.com>
2018-05-20 22:24:40 +0800

    简单修改了一下application的启动姿势

--- spring-boot-jdbc/src/main/java/com/chenjie/springbootjdbc/SpringBootJdbcApplication.java
+++ spring-boot-jdbc/src/main/java/com/chenjie/springbootjdbc/SpringBootJdbcApplication.java
@@ -3,10 +3,17 @@ package com.chenjie.springbootjdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chenjie
+ */
 @SpringBootApplication
 public class SpringBootJdbcApplication {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(SpringBootJdbcApplication.class, args);
+    }*/
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringBootJdbcApplication.class);
+        application.run(args);
     }
 }
