e6ba81724065a23a584f07d190f4e6c2e5e16f16
myGit <729217667@qq.com>
2018-05-22 23:54:29 +0800

    新建一个springboot项目,用来整合beatlsql

---
+++ springboot-beatlsql/src/main/java/com/chenjie/springbootbeatlsql/SpringbootBeatlsqlApplication.java
@@ -0,0 +1,12 @@
+package com.chenjie.springbootbeatlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeatlsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBeatlsqlApplication.class, args);
+    }
+}
