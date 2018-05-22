59b3532351095724c8b69cff096685100e3e8e9b
myGit <729217667@qq.com>
2018-05-22 23:03:03 +0800

    再次简单修改了一下application的启动姿势

--- spring-boot-jdbc/src/main/java/com/chenjie/springbootjdbc/SpringBootJdbcApplication.java
+++ spring-boot-jdbc/src/main/java/com/chenjie/springbootjdbc/SpringBootJdbcApplication.java
@@ -1,7 +1,12 @@
 package com.chenjie.springbootjdbc;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 /**
  * @author chenjie
@@ -14,6 +19,13 @@ public class SpringBootJdbcApplication {
     }*/
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringBootJdbcApplication.class);
+
+        application.setBanner((environment,sourceClass,out)->{
+            System.out.println("      _      _       _\n" +                       "     | |    (_)     | |\n" +                       " ____| |__   _  ___ | |__    ___  _ __    __ _\n" +                       "|_  /| '_ \\ | |/ __|| '_ \\  / _ \\| '_ \\  / _` |\n" +                       " / / | | | || |\\__ \\| | | ||  __/| | | || (_| |\n" +                       "/___||_| |_||_||___/|_| |_| \\___||_| |_| \\__, |\n" +                       "                                          __/ |\n" +                       "                                         |___/\n");
+        });
+
+        application.setBannerMode(Banner.Mode.CONSOLE);
+
         application.run(args);
     }
 }
